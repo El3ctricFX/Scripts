8bee5f2f2d04bba1140304348ede5273ce0440f8
Electric <173574107+El3ctricFX@users.noreply.github.com>
2025-05-17 23:26:04 -0400

    screenshot

---
+++ screenshot.sh
@@ -0,0 +1,14 @@
+
+date_formatted=$(date +%Y-%m-%d)
+counter=1
+
+
+while [[ -f ~/Pictures/${date_formatted}-${counter}.png ]]; do
+  ((counter++))
+done
+
+
+grim -g "$(slurp)" ~/Pictures/${date_formatted}-${counter}.png
+
+
+swappy -f ~/Pictures/${date_formatted}-${counter}.png 
